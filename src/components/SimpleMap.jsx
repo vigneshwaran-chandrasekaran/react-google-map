ea04deb3a894fdcfdbf1116533d7a365e93e09de
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-09-29 19:36:36 +0530

    added bootstrap packages

--- src/components/SimpleMap.jsx
+++ src/components/SimpleMap.jsx
@@ -1,32 +1,57 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import marker from '../assets/images/images.png';
+// import icon from '../../public/logo192.png';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => {
+    return (
+        <div>
+            <span style={{ color: 'green' }}>{text}</span>
+            <img src={marker} alt='map-icon' width='30' height='30' />
+        </div>
+    )
+};
+const TestingComponent = ({ text }) => <div><p style={{ color: 'red' }}>Testing component</p></div>;
 
 class SimpleMap extends Component {
     static defaultProps = {
         center: {
-            lat: 59.95,
-            lng: 30.33
+            lat: 10.805,
+            lng: 78.685277777778
         },
         zoom: 11
     };
 
+    componentDidMount() {
+        // console.log({ icon });
+        console.log('process.env', process.env) // expected 'staging' to be printed here when running `npm run staging`
+        console.log('process.env.NODE_ENV', process.env.NODE_ENV) // expected 'staging' to be printed here when running `npm run staging`
+    }
+
     render() {
         return (
             // Important! Always set the container height explicitly
-            <div style={{ height: '100vh', width: '100%' }}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: 'AIzaSyBrUW0Q1Wu9pwK2OdkD2kqMlwdUwbSe_eM' }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                >
-                    <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text="My Marker"
-                    />
-                </GoogleMapReact>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col'></div>
+                    <div className='col'>
+                        <div style={{ height: '100vh', width: '100%' }}>
+                            <GoogleMapReact
+                                bootstrapURLKeys={{ key: 'AIzaSyBrUW0Q1Wu9pwK2OdkD2kqMlwdUwbSe_eM' }}
+                                defaultCenter={this.props.center}
+                                defaultZoom={this.props.zoom}
+                            >
+                                <TestingComponent />
+                                <AnyReactComponent
+                                    lat={10.805}
+                                    lng={78.685277777778}
+                                    text="Trichy My City"
+                                />
+                                <TestingComponent />
+                            </GoogleMapReact>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
